2af505536a74abef8139734290b5679ad9e4f316
Umut Seven <umutseven92@gmail.com>
2018-02-27 01:52:56 +0300

    Added main page as a template

--- SWE573/pulse/views.py
+++ SWE573/pulse/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def index(request):
-	return HttpResponse("Hello world. You are at the pulse index.")
+	return render(request, "pulse/index.html")
