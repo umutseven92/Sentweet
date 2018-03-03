572b46194a41120cc92a357bf344d8783617ada3
Umut Seven <umutseven92@gmail.com>
2018-03-03 20:08:19 +0300

    Added positive & negative words, created twitter API backbone

--- SWE573/SWE573/settings.py
+++ SWE573/SWE573/settings.py
@@ -118,3 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
+STATICFILES_DIRS = (
+  os.path.join(SITE_ROOT, 'static/'),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
--- SWE573/pulse/views.py
+++ SWE573/pulse/views.py
@@ -18,6 +18,37 @@ def index(request):
     if cred is None:
         raise Exception("Could not authorize user. Please double check your credentials.")
 
-    tweets = api.GetSearch(term="beyonce", count=100, include_entities=False, return_json=True)
-    context = {"tweets": tweets}
+    term = 'beyonce'
+    positive_score = 0
+    negative_score = 0
+    error = ''
+
+    tweets = api.GetSearch(term=term, count=100, include_entities=False)
+
+    if len(tweets) <= 0:
+        error = 'No tweets could be found about ' + term + '.'
+    else:
+        current_folder = os.path.dirname(os.path.realpath('__file__'))
+        positive_words = load_words(os.path.join(current_folder, 'pulse/sentiment-analysis/positive-words.txt'))
+        negative_words = load_words(os.path.join(current_folder, 'pulse/sentiment-analysis/negative-words.txt'))
+
+        for tweet in tweets:
+            text = tweet.text.split(' ')
+
+            for word in text:
+                if word in positive_words:
+                    positive_score += 1
+                if word in negative_words:
+                    negative_score += 1
+
+    total_score = positive_score - negative_score
+    context = {'search_term': term, 'positive_score': positive_score,
+               'negative_score': negative_score, 'total_score': total_score,
+               'error': error}
     return render(request, "pulse/index.html", context)
+
+
+def load_words(path):
+    text_file = open(path, 'r')
+    lines = text_file.read().splitlines()
+    return lines
