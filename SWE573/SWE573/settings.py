4b3c0c6f5d154062231ade5d59976062705eb6de
Umut Seven <umutseven92@gmail.com>
2018-03-03 18:09:52 +0300

    PEP8 standarts, changed Django secret key

--- SWE573/SWE573/settings.py
+++ SWE573/SWE573/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'v=hrsyc#!3cgd2i+$kx7e75vmq@ziuk6z1a36a^h2skwr%q&ni'
+SECRET_KEY = os.environ['djangoSecretKey']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- SWE573/pulse/views.py
+++ SWE573/pulse/views.py
@@ -2,21 +2,22 @@ from django.shortcuts import render
 import os
 import twitter
 
+
 def index(request):
-	consumerKey = os.environ["consumerKey"]
-	consumerSecret = os.environ["consumerSecret"]
-	accessTokenKey = os.environ["accessTokenKey"]
-	accessTokenSecret = os.environ["accessTokenSecret"]
- 
-	api = twitter.Api(consumer_key=consumerKey,
-			consumer_secret=consumerSecret,
-			access_token_key=accessTokenKey,
-			access_token_secret=accessTokenSecret)
+    consumer_key = os.environ["consumerKey"]
+    consumer_secret = os.environ["consumerSecret"]
+    access_token_key = os.environ["accessTokenKey"]
+    access_token_secret = os.environ["accessTokenSecret"]
 
-	cred = api.VerifyCredentials()
-	if cred is None:
-		raise Exception("Could not authorize user. Please double check your credentials.")
+    api = twitter.Api(consumer_key=consumer_key,
+                      consumer_secret=consumer_secret,
+                      access_token_key=access_token_key,
+                      access_token_secret=access_token_secret)
 
-	tweets = api.GetSearch(term="beyonce",count=100,include_entities=False, return_json=True)
-	context = {"tweets":tweets}
-	return render(request, "pulse/index.html", context)
+    cred = api.VerifyCredentials()
+    if cred is None:
+        raise Exception("Could not authorize user. Please double check your credentials.")
+
+    tweets = api.GetSearch(term="beyonce", count=100, include_entities=False, return_json=True)
+    context = {"tweets": tweets}
+    return render(request, "pulse/index.html", context)
