f8c33ba61b4c40258856ad78a43c89ef9d2fb80f
Umut Seven <umutseven92@gmail.com>
2018-03-03 18:01:58 +0300

    Added first twitter API call

--- SWE573/SWE573/urls.py
+++ SWE573/SWE573/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+	path('', include('pulse.urls')),
 	path('pulse/', include('pulse.urls')),
 	path('admin/', admin.site.urls),
 ]
--- SWE573/pulse/views.py
+++ SWE573/pulse/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
-
+import os
+import twitter
 
 def index(request):
-	return render(request, "pulse/index.html")
+	consumerKey = os.environ["consumerKey"]
+	consumerSecret = os.environ["consumerSecret"]
+	accessTokenKey = os.environ["accessTokenKey"]
+	accessTokenSecret = os.environ["accessTokenSecret"]
+ 
+	api = twitter.Api(consumer_key=consumerKey,
+			consumer_secret=consumerSecret,
+			access_token_key=accessTokenKey,
+			access_token_secret=accessTokenSecret)
+
+	cred = api.VerifyCredentials()
+	if cred is None:
+		raise Exception("Could not authorize user. Please double check your credentials.")
+
+	tweets = api.GetSearch(term="beyonce",count=100,include_entities=False, return_json=True)
+	context = {"tweets":tweets}
+	return render(request, "pulse/index.html", context)
