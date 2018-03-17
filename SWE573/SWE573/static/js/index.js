0578186424a34bd71848fc4f7ad5472ed7d39871
Umut Seven <umutseven92@gmail.com>
2018-03-17 03:36:56 +0300

    More work on UI

--- SWE573/SWE573/static/js/index.js
+++ SWE573/SWE573/static/js/index.js
@@ -1,7 +1,18 @@
-
 function drawChart(result) {
     var res = htmlDecode(result).replace(/'/g, "\"");
     var obj = JSON.parse(res);
+
+    var neg_array = [];
+    var pos_array = [];
+    var neu_array = [];
+    var comp_array = [];
+
+    for (var i = 0; i < Object.keys(obj).length; i++){
+        pos_array[i] = Object.values(obj)[i]["pos"];
+        neu_array[i] = Object.values(obj)[i]["neu"];
+        comp_array[i] = Object.values(obj)[i]["compound"];
+        neg_array[i] = Object.values(obj)[i]["neg"];
+    }
 
     var ctx = document.getElementById("result_chart").getContext('2d');
 
@@ -9,19 +20,42 @@ function drawChart(result) {
         type: 'line',
         data: {
             labels: Object.keys(obj),
-            datasets:[{
-                label: 'Compound Score',
-                backgroundColor: '#008000',
-                borderColor:'#008000',
-                data: Object.values(obj),
-                fill: false
-            }]
+            datasets: [
+                {
+                    label: 'Positivity Score',
+                    backgroundColor: '#008000',
+                    borderColor: '#008000',
+                    data: pos_array,
+                    fill: false
+                },
+                {
+                    label: 'Negativity Score',
+                    backgroundColor: '#ff0000',
+                    borderColor: '#ff0000',
+                    data: neg_array,
+                    fill: false
+                },
+                {
+                    label: 'Neutrality Score',
+                    backgroundColor: '#a8adb4',
+                    borderColor: '#a8adb4',
+                    data: neu_array,
+                    fill: false
+                },
+                {
+                    label: 'Compound Score',
+                    backgroundColor: '#ffff00',
+                    borderColor: '#ffff00',
+                    data: comp_array,
+                    fill: false
+                }
+            ]
         }
     })
 }
 
-function htmlDecode(input){
-  var e = document.createElement('div');
-  e.innerHTML = input;
-  return e.childNodes[0].nodeValue;
+function htmlDecode(input) {
+    var e = document.createElement('div');
+    e.innerHTML = input;
+    return e.childNodes[0].nodeValue;
 }
--- SWE573/pulse/views.py
+++ SWE573/pulse/views.py
@@ -39,18 +39,36 @@ def index(request):
 
             new_since_str = new_since.strftime('%Y-%m-%d')
             new_until_str = new_until.strftime('%Y-%m-%d')
-            tweets = api.GetSearch(term=term, count=100, include_entities=False, since=new_since_str, until=new_until_str,
+            tweets = api.GetSearch(term=term, count=50, include_entities=False,
+                                   since=new_since_str, until=new_until_str,
                                    lang='en')
-            compound = 0
+
+            positive, negative, neutral, compound = 0, 0, 0, 0
+
             for tweet in tweets:
                 tweet_body = tweet.text
                 sid = SentimentIntensityAnalyzer()
                 ss = sid.polarity_scores(tweet_body)
-                comp = ss['compound']
-                compound = compound + comp
+                compound = compound + ss['compound']
+                positive = positive + ss['pos']
+                negative = negative + ss['neg']
+                neutral = neutral + ss['neu']
 
-            average_comp = compound / len(tweets)
-            result[new_since_str] = average_comp
+            tweet_count = len(tweets)
+
+            if tweet_count == 0:
+                average_comp = 0
+                average_pos = 0
+                average_neg = 0
+                average_neu = 0
+            else:
+                average_comp = compound / tweet_count
+                average_pos = positive / tweet_count
+                average_neg = negative / tweet_count
+                average_neu = neutral / tweet_count
+
+            result[new_since_str] = {'pos': average_pos, 'neg': average_neg,
+                                     'neu': average_neu, 'compound': average_comp}
 
         context = {'result': result}
         return render(request, "pulse/index.html", context)
