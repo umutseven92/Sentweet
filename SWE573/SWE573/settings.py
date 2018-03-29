049c57bfc548e75b47273f2f08c05da6620fcf6f
Umut Seven <umutseven92@gmail.com>
2018-03-30 01:29:54 +0300

    settings.py change for prod

--- SWE573/SWE573/settings.py
+++ SWE573/SWE573/settings.py
@@ -122,7 +122,7 @@ STATIC_URL = '/static/'
 
 SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
 STATICFILES_DIRS = (
-  os.path.join(SITE_ROOT, 'static/'),
+  os.path.join(BASE_DIR, 'static/'),
 )
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
