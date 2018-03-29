939976b423c2806fddb7dd7094e37ee7b44ca428
Umut Seven <umutseven92@gmail.com>
2018-03-30 01:19:20 +0300

    settings.py change for prod

--- SWE573/SWE573/settings.py
+++ SWE573/SWE573/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['djangoSecretKey']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ['djangoDebug']
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
