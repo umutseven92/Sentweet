6c5dff0801f0d51a62a6a8545b1465c0b0e816f8
Umut Seven <umutseven92@gmail.com>
2018-04-05 00:56:13 +0300

    Started moving to jquery ui datepicker

--- SWE573/SWE573/static/js/index.js
+++ SWE573/SWE573/static/js/index.js
@@ -1,8 +1,27 @@
+$(function () {
+    $("#start_date_input").datepicker({
+        dateFormat: 'yy-mm-dd',
+        maxDate: new Date(),
+        minDate: new Date(2006, 3, 21),
+        onSelect: function (dateText) {
+            var weekAgo = new Date(new Date().setDate($("#end_date_input").datepicker("getDate") - 7));
+            $("#end_date_input").datepicker('option', 'maxDate', weekAgo);
+        }
+    }).datepicker("setDate", new Date());
+
+    var weekAgo = new Date(new Date().setDate(new Date().getDate()-7));
+    $("#end_date_input").datepicker({
+        dateFormat: 'yy-mm-dd',
+        maxDate: weekAgo,
+        minDate: new Date(2006, 3, 21)
+    }).datepicker("setDate", weekAgo);
+});
+
 $(window).on('load', function () {
     $('#loading').hide();
 });
 
-$('#main_form').submit(function(){
+$('#main_form').submit(function () {
     $('#loading').show();
 });
 
